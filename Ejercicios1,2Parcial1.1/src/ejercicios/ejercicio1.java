319cd869b8bd7c5e06ba3fc9921fcd0fca76773d
G30 <gfloriane@miumg.edu.gt>
2026-03-14 14:40:50 -0600

    segunda parte subida

--- Ejercicios1,2Parcial1.1/src/ejercicios/ejercicio1.java
+++ Ejercicios1,2Parcial1.1/src/ejercicios/ejercicio1.java
@@ -4,3 +4,13 @@ public class ejercicio1 {
 
     public static int calcularPuntaje(int[] numeros) {
         int puntajeTotal = 0;
+        
+        for (int numero : numeros) {
+            if (numero == 5) {
+                puntajeTotal += 5;
+            } else if (numero % 2 == 0) {
+                puntajeTotal += 1;
+            } else {
+                puntajeTotal += 3;
+            }
+        }

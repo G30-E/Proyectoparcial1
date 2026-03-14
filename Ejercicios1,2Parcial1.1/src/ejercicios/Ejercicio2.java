c7eb6426764d6f9b1010b1b65e02dcb264185869
G30 <gfloriane@miumg.edu.gt>
2026-03-14 14:54:11 -0600

    primera parte ejercicio 2

---
+++ Ejercicios1,2Parcial1.1/src/ejercicios/Ejercicio2.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+import java.util.Arrays;
+
+public class Ejercicio2 {
+
+    public static int[] secondminmax(int[] numbers) {
+        int menor = Integer.MAX_VALUE;
+        int segundoMenor = Integer.MAX_VALUE;
+
+        int mayor = Integer.MIN_VALUE;
+        int segundoMayor = Integer.MIN_VALUE;

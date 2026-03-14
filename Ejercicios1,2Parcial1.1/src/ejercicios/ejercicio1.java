f816cb1c68bc37531d78b42df6ee455ea8f88968
G30 <gfloriane@miumg.edu.gt>
2026-03-14 14:44:10 -0600

    tercera parte subida ejercicio 1

--- Ejercicios1,2Parcial1.1/src/ejercicios/ejercicio1.java
+++ Ejercicios1,2Parcial1.1/src/ejercicios/ejercicio1.java
@@ -1,4 +1,4 @@
-package ejerciciosparcial1;
+package ejercicios;
 
 public class ejercicio1 {
 
@@ -14,3 +14,25 @@ public class ejercicio1 {
                 puntajeTotal += 3;
             }
         }
+
+        return puntajeTotal;
+    }
+
+    public static void main(String[] args) {
+        int[] arreglo1 = {1, 2, 3, 4, 5};
+        int[] arreglo2 = {17, 19, 21};
+        int[] arreglo3 = {5, 5, 5};
+
+        System.out.println("Resultado 1: " + calcularPuntaje(arreglo1));
+        System.out.println("Resultado 2: " + calcularPuntaje(arreglo2));
+        System.out.println("Resultado 3: " + calcularPuntaje(arreglo3));
+
+        System.out.println("Complejidad temporal: O(n)");
+        System.out.println("Complejidad espacial: O(1)");
+    }
+}
+
+
+//Justificación breve:
+//Es O(n) en tiempo porque solo se hace una pasada sobre el arreglo.
+//Es O(1) en espacio porque solo se usan variables simples adicionales.

03983358a49d66a11b1848b66b12510b3bdfa56d
G30 <gfloriane@miumg.edu.gt>
2026-03-14 15:09:24 -0600

    ejercicio 2 terminado

--- Ejercicios1,2Parcial1.1/src/ejercicios/Ejercicio2.java
+++ Ejercicios1,2Parcial1.1/src/ejercicios/Ejercicio2.java
@@ -4,9 +4,40 @@ import java.util.Arrays;
 
 public class Ejercicio2 {
 
-    public static int[] secondminmax(int[] numbers) {
+    public static int[] secondMinMax(int[] numbers) {
         int menor = Integer.MAX_VALUE;
         int segundoMenor = Integer.MAX_VALUE;
-
         int mayor = Integer.MIN_VALUE;
         int segundoMayor = Integer.MIN_VALUE;
+
+        for (int numero : numbers) {
+
+  
+            if (numero < menor) {
+                segundoMenor = menor;
+                menor = numero;
+            } else if (numero > menor && numero < segundoMenor) {
+                segundoMenor = numero;
+            }
+
+        
+            if (numero > mayor) {
+                segundoMayor = mayor;
+                mayor = numero;
+            } else if (numero < mayor && numero > segundoMayor) {
+                segundoMayor = numero;
+            }
+        }
+
+        return new int[]{segundoMenor, segundoMayor};
+    }
+
+    public static void main(String[] args) {
+        int[] arreglo = {7, 2, 9, 4, 1, 8};
+
+        int[] resultado = secondMinMax(arreglo);
+
+        System.out.println("Arreglo original: " + Arrays.toString(arreglo));
+        System.out.println("Resultado: " + Arrays.toString(resultado));
+    }
+}
